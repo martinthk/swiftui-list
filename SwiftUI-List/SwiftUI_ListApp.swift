9e73105291fcedf51cf4329ab422d083fe613e2f
Martin T <martintom.work@gmail.com>
2021-04-29 10:28:59 +0800

    Initial Commit

---
+++ SwiftUI-List/SwiftUI_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ListApp.swift
+//  SwiftUI-List
+//
+//  Created by Martin Tom on 29/4/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
